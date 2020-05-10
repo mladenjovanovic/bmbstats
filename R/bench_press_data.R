b72cbb05dfb5d0bd8dc48349e40a84cf500dabf2
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2020-05-10 10:20:52 +0200

    Added bench press data

---
+++ R/bench_press_data.R
@@ -0,0 +1,16 @@
+#' Bench press data
+#' 
+#' Effects of training intervention using N=20 subject on bench press 1RM
+#' 
+#' @format A data frame with 20 rows and 5 variables:
+#' \describe{
+#'   \item{Athlete}{Name of the athlete}
+#'   \item{Pre-test}{Bench press 1RM Pre-test results in kg}
+#'   \item{Post-test}{Bench press 1RM Post-test results in kg}
+#'   \item{Change}{Difference between Post-test and Pre-test}
+#'   \item{Magnitude}{Factor indicating magnitude of the change based on
+#'                    the SESOI band (+/- 5kg). Three levels: Lower, Equivalent,
+#'                    and Higher.}
+#' }
+#' @usage data(bench_press_data)
+"bench_press_data"
---
+++ data-raw/bench_press_data.R
@@ -0,0 +1,45 @@
+## code to prepare `bench_press_data` dataset
+require(tidyverse)
+
+set.seed(1667)
+
+n_subjects <- 20
+
+SESOI_lower <- -5 #kg
+SESOI_upper <- 5 #kg
+
+bench_press_data <- tibble(
+  Athlete = paste(
+    "Athlete",
+    str_pad(
+      string = seq(1, n_subjects),
+      width = 2,
+      pad = "0"
+      )
+    ),
+  `Pre-test` = rnorm(
+    n = n_subjects,
+    mean = 100,
+    sd = 7.5
+    ),
+  `Post-test` = `Pre-test` + rnorm(n = n_subjects, mean = 5, sd = 10),
+  `Change` = `Post-test` -`Pre-test`,
+  Magnitude = ifelse(
+    `Change` > SESOI_upper,
+    "Higher",
+    ifelse(
+      `Change` < SESOI_lower,
+      "Lower",
+      "Equivalent"
+      )
+    )
+  )
+
+bench_press_data$Magnitude <- factor(
+  bench_press_data$Magnitude,
+  levels = c("Lower", "Equivalent", "Higher")
+  )
+
+
+usethis::use_data(bench_press_data, overwrite = TRUE)
+
